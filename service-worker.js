925fcd34cd02ea49046a63d4e0789603410fde6c
boteandopiola <85791254+boteandopiola@users.noreply.github.com>
2026-04-28 11:30:25 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,19 @@
-const CACHE_NAME = "notas-app-v2"; // cambiar la versión para actualizar no olvidar
-
+const CACHE_NAME = "notas-app";
 const urlsToCache = [
   "./",
   "./index.html"
 ];
 
 self.addEventListener("install", event => {
-  self.skipWaiting(); // fuerza instalación inmediata
-
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache); //  borra cachés viejos
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
-});
-
 self.addEventListener("fetch", event => {
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
