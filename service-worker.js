e5ad069206d5e442c3e126cfa5026ea15eef3c0e
boteandopiola <85791254+boteandopiola@users.noreply.github.com>
2026-05-13 01:09:18 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,52 @@
-onst CACHE_NAME = "notas-app-v3"; // cambiar la versión para actualizar no olvidar
+const CACHE_NAME = "notas-app-v4";
 
 const urlsToCache = [
   "./",
-  "./index.html"
+  "./index.html",
 ];
 
-self.addEventListener("install", event => {
-  self.skipWaiting(); // fuerza instalación inmediata
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
 
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("activate", event => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => {
           if (cache !== CACHE_NAME) {
-            return caches.delete(cache); //  borra cachés viejos
+            return caches.delete(cache);
           }
         })
-      );
-    })
+      )
+    ).then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
+
+  // SOLO GET
+  if (event.request.method !== "GET") return;
+
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    fetch(event.request)
+      .then((response) => {
+
+        // guardar copia nueva
+        const responseClone = response.clone();
+
+        caches.open(CACHE_NAME)
+          .then((cache) => {
+            cache.put(event.request, responseClone);
+          });
+
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
